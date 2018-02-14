2e1770f54bd4f7379e587bf329edb9eed19f729c
Vianney Dupoy de Guitard <vianney.deguitard@lyra-network.com>
2018-02-14 16:21:40 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,24 @@
+let args = process.argv.slice(2)
+let path = args[0]
+
+if (path === undefined) {
+    console.log('Usage: node index /path/to/scan');
+    return -1
+}
+
+let git = require('simple-git')(path)
+let recursive = require("recursive-readdir")
+let exec = require('child_process').exec
+
+let ignoreList = ['.git', 'target', 'node_modules']
+let handleFile = (filepath) => {
+    exec('wc -l < ' + filepath, function (error, lineCount) {
+        git.log({file: filepath}, (err, log) => {
+            console.log(`${filepath};${log.all.length};${lineCount.trim()}`);
+        });
+    });
+}
+
+recursive(path, ignoreList, function (err, files) {
+	files.forEach(handleFile);
+});
