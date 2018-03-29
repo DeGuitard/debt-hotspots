cb848d3e38d0f1b8ea501d04e2d05753ecf4129a
Vianney Dupoy de Guitard <vianney.deguitard@lyra-network.com>
2018-03-29 09:24:08 +0200

    Rewrote implementation (optimization & hierarchical output)

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-let args = process.argv.slice(2)
-let path = args[0]
+const dirTree = require('directory-tree'),
+      exec = require('child_process').exec
+
+let args = process.argv.slice(2),
+    path = args[0],
+    operations = []
 
 if (path === undefined) {
-    console.log('Usage: node index /path/to/scan');
+    console.log('Usage: node index /path/to/scan')
     return -1
 }
 
-let git = require('simple-git')(path)
-let recursive = require("recursive-readdir")
-let exec = require('child_process').exec
-
-let ignoreList = ['.git', 'target', 'node_modules']
-let handleFile = (filepath) => {
-    exec('wc -l < ' + filepath, function (error, lineCount) {
-        git.log({file: filepath}, (err, log) => {
-            if (log.all.length > 0) {
-                console.log(`${filepath.replace(path, '')};${log.all.length};${lineCount.trim()}`);
-            }
-        });
-    });
+/** Computes score asynchronously, formula is: commit count * (line count / 2). */
+let computeScore = function(node) {
+    return new Promise((resolve, reject) => {
+        exec('wc -l < ' + node.path, function (error, lineCount) {
+            exec(`git -C ${path} rev-list --count HEAD ${node.path}`, function(error, commitCount) {
+                node.commits = commitCount.trim()
+                node.lines = lineCount.trim()
+                node.score = node.commits * (node.lines / 2)
+                resolve()
+            })
+        })
+    })
 }
 
-recursive(path, ignoreList, function (err, files) {
-	files.forEach(handleFile);
-});
+/** Computes score sums for each folder, representing the sum of all its leaves. */
+let sumScores = function(obj) {
+    if (obj.type === 'file') return obj.score
+    else obj.score = 0
+    obj.children.forEach(child => {
+        child.score = sumScores(child)
+        obj.score += child.score
+    })
+    return obj.score
+}
+
+let printResults = function(tree) {
+    let strResult = JSON.stringify(tree)
+    console.log(strResult);
+}
+
+const ignoreList = [/\.git/, /target/, /node_modules/]
+let tree = dirTree(path,
+    {exclude: ignoreList},
+    (node) => operations.push(computeScore(node))
+)
+
+Promise.all(operations)
+    .then(() => sumScores(tree))
+    .then(() => printResults(tree))
